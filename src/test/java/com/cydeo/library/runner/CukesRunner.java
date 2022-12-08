17ee954ac7b05e7f24097f5fdb921b6c85c54ae1
Vitalii Efimov <g3770888@gmail.com>
2022-12-08 09:42:10 -0500

    new commit

--- src/test/java/com/cydeo/library/runner/CukesRunner.java
+++ src/test/java/com/cydeo/library/runner/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
     @CucumberOptions(
             plugin = {
                     "pretty",
-                    "json:target/cucumber.json",
+                    "json:target/cucumber-reports/cucumber.json",
                     "html:target/cucumber-report.html",
                     "rerun:target/rerun.txt",
                     "me.jvt.cucumber.report.PrettyReports:target/cucumber"
