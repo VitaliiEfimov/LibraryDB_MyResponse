b2bc3f38ee8f4e8a9e71878e076bb2c750534472
Vitalii Efimov <g3770888@gmail.com>
2022-12-16 21:31:19 -0500

    new commit

--- src/test/java/com/cydeo/library/utilities/Driver.java
+++ src/test/java/com/cydeo/library/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.cydeo.library.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,24 +13,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    private Driver() {}
+    private Driver() {
+    }
 
     private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
     public static WebDriver getDriver() {
-        if(driverPool.get() == null){  // if driver/browser was never opened
-            String browserType="";
-            if(System.getProperty("BROWSER")==null){
+        if (driverPool.get() == null) {  // if driver/browser was never opened
+            String browserType = "";
+            if (System.getProperty("BROWSER") == null) {
                 browserType = ConfigurationReader.getProperty("browser");
-            }else {
-                browserType=System.getProperty("BROWSER");
+            } else {
+                browserType = System.getProperty("BROWSER");
             }
-            switch(browserType){
+            switch (browserType) {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
                         String gridAddress = "3.88.45.34";//54.172.98.21
-                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
                         driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
@@ -43,7 +45,7 @@ public class Driver {
                     try {
                         // assign your grid server address
                         String gridAddress = "3.88.45.34";//54.172.98.21
-                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
                         driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
@@ -68,6 +70,15 @@ public class Driver {
                     driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
 
+                case "headless-chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions option = new ChromeOptions();
+                    option.setHeadless(true);
+                    driverPool.set(new ChromeDriver(option));
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
             }
         }
 
@@ -76,9 +87,8 @@ public class Driver {
 
     }
 
-
-    public static void closeDriver(){
-        if(driverPool.get() != null) {
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
             driverPool.get().quit(); // this line will kill the session. value will not be null
             driverPool.remove();
         }
